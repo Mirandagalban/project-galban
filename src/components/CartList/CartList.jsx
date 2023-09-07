5b59a650a77e6800897fbd0fb99bc29f98df7034
Miranda Galban <mirandagalbanbjj@gmail.com>
2023-09-07 00:15:07 -0300

    cartEditing

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,22 +1,26 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
-import './cartlist.css'
+import "./cartlist.css";
 
-const CartList = ({item}) => {
-    const {deleteItem}= useContext(CartContext)
-return (
-   <div className="cart-list">
-            <div className="cart-list-table mt-2">
-                <img src={item.imagen} alt={item.nombre} width={90}/>
-                <p>{item.nombre}</p>
-                <p>Precio: ${item.precio}</p>
-                <p>Cantidad seleccionada: {item.quantity}</p>
-                <p>Subtotal: ${item.quantity*item.precio}</p>
-                <button className="btn btn-danger btn-x " onClick={()=> deleteItem(item.id)}>X</button>
-                </div>
+const CartList = ({ item }) => {
+  const { deleteItem } = useContext(CartContext);
+  return (
+    <div className="cart-list">
+      <div className="cart-list-table mt-2">
+        <img src={item.imagen} alt={item.nombre} width={90} />
+        <p>{item.nombre}</p>
+        <p>Precio: ${item.precio}</p>
+        <p>Cantidad seleccionada: {item.quantity}</p>
+        <p>Subtotal: ${item.quantity * item.precio}</p>
+        <button
+          className="btn btn-danger btn-x "
+          onClick={() => deleteItem(item.id)}
+        >
+          X
+        </button>
+      </div>
     </div>
-   )
-}
+  );
+};
 
-export default CartList
+export default CartList;
